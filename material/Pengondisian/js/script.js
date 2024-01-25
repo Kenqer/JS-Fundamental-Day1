706c726b432fb3cb19f3f08204aab68c52d6e8ce
kenqer <javierkennedi09@gmail.com>
2024-01-25 15:30:50 +0700

    Materi(Pengondisian+Aritmatika)

---
+++ material/Pengondisian/js/script.js
@@ -0,0 +1,20 @@
+let nilai = 85;
+
+if (nilai > 85 &&  nilai < 100){
+    console.log('A')
+}
+else if (nilai > 75 &&  nilai < 85){
+    console.log('B')
+}
+else if (nilai > 55 &&  nilai < 75){
+    console.log('C')
+}
+else if (nilai > 48 &&  nilai < 54){
+    console.log('D')
+}
+else if (nilai > 56 &&  nilai < 48){
+    console.log('E')
+}
+else{
+    console.log('Nilai tidak valid')
+}
---
+++ material/aritmatika/js/script.js
@@ -0,0 +1,4 @@
+console.log("perkalian >>", 1 * 2);
+console.log("pembagian >>", 6 / 2);
+console.log("pengurangan >>", 1 - 2);
+console.log("sisa bagi >>", 4 % 2);
